55f34d6d1f89b09213cb770e07c3394ae9258077
Corie Stewart <coriestewart1989>
2021-06-27 08:52:34 -0400

    completed

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,17 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig.js");
+// const helpers = require("../../data/helpers/booleanHelpers.js");
+
+const findResources = () => {
+  return db("resources");
+};
+
+const createResources = async (project) => {
+  const [resource_id] = await db("resources").insert(project);
+  return findResources().where({ resource_id });
+};
+
+module.exports = {
+  findResources,
+  createResources,
+};
--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 // build your server here and require it from index.js
+// build your server here and require it from index.js
+const express = require("express");
+const helmet = require("helmet");
+
+const ProjectRouter = require("./project/router.js");
+const ResourceRouter = require("./resource/router.js");
+const TaskRouter = require("./task/router.js");
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use("/api", ProjectRouter);
+server.use("/api", ResourceRouter);
+server.use("/api", TaskRouter);
+
+server.get("/api", (req, res) => {
+  res.status(200).json({ api: "This challenge is up and running my dude!" });
+});
+
+module.exports = server;
